b4c8e6c403754577e8aef97f1fcf4f65dd988cdd
CSSym <49990625+CSSym@users.noreply.github.com>
2021-10-14 12:48:59 +0600

    Add files via upload

---
+++ Multiple JS file in one JS file/a.js
@@ -0,0 +1,5 @@
+let dv1 = document.getElementById("dv1");
+
+function abc() {
+dv1.classList.toggle("dv1");
+}
---
+++ Multiple JS file in one JS file/b.js
@@ -0,0 +1,5 @@
+let dv2 = document.getElementById("dv2");
+
+function def() {
+dv2.classList.toggle("dv2");
+}
